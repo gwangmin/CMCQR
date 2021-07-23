0ad285c0a90550f19a4c747ea57bf1b7708df844
ygm <ygm.gwangmin@gmail.com>
2021-07-23 23:18:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ def get_qr():
     Get CMC QR data
     using headless chrome, selenium
 
-    return: number code(qr content), base64 encoded qr
+    return: number code(qr content), qr img tag source code, base64 encoded qr
     '''
     # virtual display for screenless server
     # https://somjang.tistory.com/entry/Ubuntu-Ubuntu-%EC%84%9C%EB%B2%84%EC%97%90-Selenium-%EC%84%A4%EC%B9%98%ED%95%98%EA%B3%A0-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
-    from pyvirtualdisplay import Display
-    display = Display(visible=0, size=(1920, 1080))
-    display.start()
+    #from pyvirtualdisplay import Display
+    #display = Display(visible=0, size=(1920, 1080))
+    #display.start()
 
     # chrome options
     options = webdriver.ChromeOptions()
@@ -55,15 +55,24 @@ def get_qr():
     driver.get(URL)
 
     # form 입력
-    # 개인정보...
-    driver.find_element_by_css_selector('#user_no1').send_keys('')
-    driver.find_element_by_css_selector('#user_no2').send_keys('')
-    driver.find_element_by_css_selector('#user_name').send_keys('')
-    driver.find_element_by_css_selector('#user_selphone2').send_keys('')
-    driver.find_element_by_css_selector('#user_selphone3').send_keys('')
-    chkA = driver.find_element_by_css_selector('#chkA1')
-    chkB = driver.find_element_by_css_selector('#chkB2')
-    chkC = driver.find_element_by_css_selector('#chkC2')
+    #################### 개인정보 입력 ####################
+    data = {
+        '주민등록번호 앞자리' : '000000', '주민등록번호 뒷자리' : '0000000',
+        '이름' : '홍길동',
+        '전화번호1' : '010', '전화번호2' : '0000', '전화번호3' : '0000',
+        '질문1' : 1,
+        '질문2' : 1,
+        '질문3' : 1
+    }
+
+    driver.find_element_by_css_selector('#user_no1').send_keys(data['주민등록번호 앞자리'])
+    driver.find_element_by_css_selector('#user_no2').send_keys(data['주민등록번호 뒷자리'])
+    driver.find_element_by_css_selector('#user_name').send_keys(data['이름'])
+    driver.find_element_by_css_selector('#user_selphone2').send_keys(data['전화번호2'])
+    driver.find_element_by_css_selector('#user_selphone3').send_keys(data['전화번호3'])
+    chkA = driver.find_element_by_css_selector('#chkA' + str(data['질문1']))
+    chkB = driver.find_element_by_css_selector('#chkB' + str(data['질문2']))
+    chkC = driver.find_element_by_css_selector('#chkC' + str(data['질문3']))
     driver.execute_script("arguments[0].click();", chkA)
     driver.execute_script("arguments[0].click();", chkB)
     driver.execute_script("arguments[0].click();", chkC)
@@ -75,11 +84,13 @@ def get_qr():
     # 숫자 코드 -> qr img
     code = page.select('#qrimg_btn > center > div.qr')[0]['title']
     # 한 번 디코딩 하면 qr.png 바이너리가 나옴
-    tmp = page.select('#qrimg_btn > center > div.qr > img')[0]['src']
+    img_tag = page.select('#qrimg_btn > center > div.qr > img')[0]
+    tmp = img_tag['src']
     qr_b64 = tmp[tmp.find(',')+1:]
 
     driver.quit()
-    return code, qr_b64
+    return code, str(img_tag), qr_b64
+
 
 def write_qr_png(qr_b64, path):
     '''
